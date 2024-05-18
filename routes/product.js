2efd5d569f5dc8483b73dfd89d5ab37985a384cd
Vishnuprasanth-mediwave <vishnu_prasanth@mindwaveventures.com>
2024-05-19 02:41:32 +0530

    product updated completed

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const productController = require("../controller/productController");
+const { productUpdateMiddleware, productAddMiddleware } = require("../middlewares/productMiddleware");
 
-router.post("/add",productController.ProductAdd);
+router.post("/add",productAddMiddleware,productController.ProductAdd);
 router.get("/list",productController.getAllProduct);
+router.put("/:id",productUpdateMiddleware,productController.productUpdate);
+router.delete("/:id",productController.productDeleted);
+
+
 module.exports = router;
